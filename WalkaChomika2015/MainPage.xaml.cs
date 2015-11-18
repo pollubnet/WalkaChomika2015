9b7e0c1990aa213bffa66b2ed8a011889d502c41
Marcin Badurowicz <m@badurowicz.net>
2015-11-18 09:18:16 +0100

    wersja ze spotkania 1
    Zwierzę.cs pochodzące z poprzedniej wersji, nowy projekt zrobiony dla Universal Windows

---
+++ WalkaChomika2015/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿#region License
+/*
+ * Written in 2014 by Marcin Badurowicz <m dot badurowicz at pollub dot pl>
+ *
+ * To the extent possible under law, the author(s) have dedicated
+ * all copyright and related and neighboring rights to this 
+ * software to the public domain worldwide. This software is 
+ * distributed without any warranty. 
+ *
+ * You should have received a copy of the CC0 Public Domain 
+ * Dedication along with this software. If not, see 
+ * <http://creativecommons.org/publicdomain/zero/1.0/>. 
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WalkaChomika
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Prywatne pola, które będą wspólne dla wszystkich funkcji (metod)
+        /// w klasie odpowiedzialnej za wyświetlanie tego okna
+        /// </summary>
+        private Zwierzę chomik;
+        private Zwierzę pies;
+
+        /// <summary>
+        /// Konstruktor, uruchamia się przy tworzeniu okna
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // tworzenie nowego obiektu i nadawanie jego cech
+            chomik = new Zwierzę();
+            chomik.HP = 5;
+            chomik.Imię = "Pucuś";
+            chomik.Mana = 0;
+            chomik.Damage = 2;
+
+            // można też utworzyć obiekt i nadawać mu cechy od razu
+            pies = new Zwierzę()
+            {
+                Imię = "Dino",
+                Damage = 5,
+                Mana = 0,
+                HP = 20
+            };
+
+        }
+
+        /// <summary>
+        /// Funkcja realizująca walkę pomiędzy zmiennymi przekazywanymi w parametrach
+        /// czyli Zwierzęciem 1 i Zwierzęciem 2. Zwierzę 1 atakuje 2.
+        /// </summary>
+        /// <param name="zwierze1"></param>
+        /// <param name="zwierze2"></param>
+        private void Walka(Zwierzę zwierze1, Zwierzę zwierze2)
+        {
+            int z = 0;
+
+
+            while (zwierze2.CzyŻyje())
+            {
+                zwierze1.Gryź(zwierze2);
+                z++;
+            }
+
+            // ustawiamy tekst w polu tekstowym
+            liczbaUgryzien.Text = z.ToString();
+        }
+
+        /// <summary>
+        /// Obsługa kliknięcia przycisku
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // uruchomienie walki pomiędzy zwierzątkami utworzonymi w konstruktorze
+            Walka(chomik, pies);
+        }
+    }
+}
---
+++ WalkaChomika2015/Zwierzę.cs
@@ -0,0 +1,79 @@
+﻿#region License
+/*
+ * Written in 2015 by Marcin Badurowicz <m dot badurowicz at pollub dot pl>
+ *
+ * To the extent possible under law, the author(s) have dedicated
+ * all copyright and related and neighboring rights to this 
+ * software to the public domain worldwide. This software is 
+ * distributed without any warranty. 
+ *
+ * You should have received a copy of the CC0 Public Domain 
+ * Dedication along with this software. If not, see 
+ * <http://creativecommons.org/publicdomain/zero/1.0/>. 
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkaChomika
+{ 
+    /// <summary>
+    /// Klasa Zwierzę, reprezentująca zwierzę bojowe
+    /// </summary>
+    class Zwierzę
+    {
+        /// <summary>
+        /// To jest tzw. właściwość. Powinno używać się właściwości zamiast pól, ale dlaczego, to już
+        /// kwestia nieco bardziej zaawansowana, więc chwilowo ją pominiemy. To konkretne pole reprezentuje
+        /// imię zwierzęcia.
+        /// </summary>
+        public string Imię { get; set; }
+
+        /// <summary>
+        /// To pole jest liczbą, która reprezentuje ile zwierzę ma punktów życia
+        /// </summary>
+        public int HP;
+
+        /// <summary>
+        /// To pole reprezentuje ilość many zwierzęcia
+        /// </summary>
+        public int Mana;
+
+        /// <summary>
+        /// To pole reprezentuje maksymalne obrażenia zadawane przez atak
+        /// </summary>
+        public int Damage;
+
+        /// <summary>
+        /// Ta funkcja zwraca, czy zwierze jeszcze żyje, opierając się na danych z tego samego obiektu.
+        /// Funkcje w klasach nazywa się metodami.
+        /// </summary>
+        /// <returns>Zwraca, czy zwierzę jeszcze żyje</returns>
+        public bool CzyŻyje()
+        {
+            if (HP > 0)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Ta funkcja atakuje inne zwierzę - podawane jest jako parametr wykonania tej funkcji.
+        /// </summary>
+        /// <param name="z">Zwierzę do zaatakowania</param>
+        public void Gryź(Zwierzę z)
+        {
+            // tworzenie generatora liczb losowych
+            Random r = new Random();
+            // losuje liczbę z zakresu od 0 do maksymalnego ataku obecnego obiektu
+            var moc = r.Next(this.Damage);
+
+            // zwierzęciu przekazanemu jako parametr odejmuje od punktów HP tyle, ile wyniosła moc ataku
+            z.HP = z.HP - moc;
+        }
+    }
+}
